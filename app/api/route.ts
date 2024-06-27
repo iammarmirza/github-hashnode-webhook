d90d24f77960c957aa9a8e2da46a6cf977a35b50
Ammar Mirza <itsammarmirza@gmail.com>
2024-06-27 13:28:01 +0530

    test

--- app/api/route.ts
+++ app/api/route.ts
@@ -2,6 +2,7 @@ export const dynamic = "force-dynamic"; // static by default, unless reading the
 
 export async function POST(request: Request) {
   const json = await request.json();
+  const header = request.headers.get('x-hashnode-signature')
   console.debug("body", json);
   return new Response(`Hello from ${json}`);
 }
