9f7e706f04bc554219d022fc9063841e25583edc
Ammar Mirza <itsammarmirza@gmail.com>
2024-06-27 13:57:12 +0530

    test

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,10 +1,48 @@
-export const dynamic = "force-dynamic"; // static by default, unless reading the request
+import { validateSignature } from "@/utils/signing";
+
+export const dynamic = "force-dynamic";
+const GITHUB_PERSONAL_ACCESS_TOKEN = process.env.GITHUB_PERSONAL_ACCESS_TOKEN as string
+const GITHUB_REPOSITORY = process.env.GITHUB_REPOSITORY as string
+const HASHNODE_SECRET_KEY = process.env.HASHNODE_SECRET_KEY as string
 
 export async function POST(request: Request) {
   const json = await request.json();
-  const header = request.headers.get('x-hashnode-signature')
   console.debug("body", json);
-  return new Response(`Hello from ${json}, header: ${header}`);
+
+  const validationResult = validateSignature({
+    incomingSignatureHeader: request.headers.get('x-hashnode-signature'),
+    payload: json,
+    secret: HASHNODE_SECRET_KEY,
+    validForSeconds: 0
+  })
+
+  if(!validationResult.isValid) {
+    console.error('Invalid signature');
+    return new Response('Invalid signature', { status: 403 })
+  }
+
+  const response = await fetch(`https://api.github.com/repos/${GITHUB_REPOSITORY}/dispatches`, {
+    method: 'POST',
+    headers: {
+        Accept: 'application/vnd.github+json',
+        Authorization: `token ${GITHUB_PERSONAL_ACCESS_TOKEN}`
+    },
+    body: JSON.stringify({
+        event_type: 'trigger',
+        client_payload: {
+            ...json.data
+        }
+    })
+  })
+
+  if(!response.ok) {
+    console.log('Failed to trigger Github action; status code:', response.status);
+    return new Response(response.statusText, { status: response.status })
+  }
+
+  return new Response('OK');
 }
+
+
 
 export const runtime = 'nodejs';
